025af60d8f710207605910a18130e1c96ed86a99
Citra Glory <citraglory@gmail.com>
2017-04-13 12:50:26 +0700

    finishing task email back end using flask mail

--- tsm.py
+++ tsm.py
@@ -4,6 +4,9 @@ from flask import render_template
 from flask import request
 from flask import url_for
 from flask_dotenv import DotEnv
+from flask_mail import Mail
+from flask_mail import Message
+from form import FeedbackForm
 
 def create_app():
 	env = DotEnv()
@@ -13,13 +16,46 @@ def create_app():
 		'DEBUG': bool,
 		'TESTING': bool
 	})
+	app.config.update(
+		#EMAIL SETTINGS
+		MAIL_SERVER='smtp.gmail.com',
+		MAIL_PORT=465,
+		MAIL_USE_SSL=True,
+		MAIL_USERNAME = 'mitratrisinergi@gmail.com',
+		MAIL_PASSWORD = 'randomlytaken#@!'
+	)
+	mail = Mail(app)
+	mail.init_app(app)
 
-	@app.route("/")
+	@app.route("/", methods=['GET', 'POST'])
 	def index():
 		"""
 			Render the view of index page.
 		"""
-		return render_template('index.html', title='Tri Sinergi Mitra', current_page='TSM')
+		form = FeedbackForm()
+		return render_template('index.html', title='Tri Sinergi Mitra', current_page='TSM', form=form)
+
+	@app.route("/send_feedback", methods=['POST'])
+	def send_feedback():
+		"""
+			Do action with python form
+		"""
+		if request.method == 'POST' :
+			#result = request.form
+			result = request.form['email']
+			msg_subject = "You got an email from {} / {} - {}".format(request.form['email'], request.form['full_name'], request.form['subject'])
+			msg_body = request.form['body']
+			send_email(msg_subject,'mitratrisinergi@gmail.com', ['mitratrisinergi@gmail.com'], msg_body)
+			return redirect(url_for('index'))
+		else:
+			flash('The message has not been sent due to unknown failure')
+			return redirect(url_for('index'))
+
+	def send_email(subject, sender, recipients, text_body):
+		msg = Message(subject, sender=sender, recipients=recipients)
+		msg.body = text_body
+		mail.send(msg)
+		return
 
 	@app.errorhandler(404)
 	def page_not_found_error(err):
@@ -34,6 +70,7 @@ def create_app():
 		Render the view of error 500 page
 		"""
 		return render_template('500.html', title='TSM internal server error', current_page='500')
+
 
 	return app
 
